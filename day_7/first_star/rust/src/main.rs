707a702c20b16c853a8604c71b965fc092a424f8
Alexandr Pleskot <alexandrpleskot@gmail.com>
2023-12-07 06:52:26 +0100

    add solution for day 7 part one incomplete

---
+++ day_7/first_star/rust/src/main.rs
@@ -0,0 +1,79 @@
+fn main() {
+    let input = "../../test.txt";
+    let input_file = std::fs::read_to_string(input).expect("Error reading input file");
+    let hands = input_file
+        .lines()
+        .map(|line| line.split_once(" ").unwrap())
+        .map(|(a, b)| {
+            (
+                a.chars().map(|c| card_to_num(c)).collect::<Vec<u64>>(),
+                b.parse::<u64>().unwrap(),
+            )
+        })
+        .collect::<Vec<_>>();
+    let mut t_hands = Vec::new();
+    for (hand, _) in &hands {
+        let mut t_hand: Vec<u64> = Vec::new();
+        t_hand.resize(13, 0);
+        for card in hand {
+            t_hand[*card as usize - 2] += 1;
+        }
+        t_hand = t_hand.into_iter().filter(|x| *x != 0).collect();
+        t_hand.sort_unstable();
+        let t_hand = match &t_hand[..] {
+            [5] => 6,
+            [1, 4] => 5,
+            [2, 3] => 4,
+            [1, 1, 3] => 3,
+            [1, 2, 2] => 2,
+            [1, 1, 1, 2] => 1,
+            [1, 1, 1, 1, 1] => 0,
+            x => {
+                println!("{:?}", x);
+                panic!("Invalid hand");
+            },
+        };
+        t_hands.push(t_hand);
+    }
+    let mut hands = hands
+        .into_iter()
+        .zip(t_hands.into_iter())
+        .collect::<Vec<_>>();
+    hands.sort_by(|((cards, _), typed), ((cards2, _), typed2)| {
+        let mut ord = typed.cmp(&typed2);
+        if ord == std::cmp::Ordering::Equal {
+            for (card, card2) in cards.iter().rev().zip(cards2.iter().rev()) {
+                ord = card.cmp(card2);
+                if ord != std::cmp::Ordering::Equal {
+                    break;
+                }
+            }
+        }
+        ord
+    });
+    let output: usize = hands.iter().enumerate().map(|(g,((cards, bet),_))|{
+        println!("{} {:?} {}", g, cards, bet);
+        (*bet as usize)*(g + 1)
+    }).sum();
+    println!("{}", output);
+    
+}
+
+fn card_to_num(card: char) -> u64 {
+    match card {
+        '2' => 2,
+        '3' => 3,
+        '4' => 4,
+        '5' => 5,
+        '6' => 6,
+        '7' => 7,
+        '8' => 8,
+        '9' => 9,
+        'T' => 10,
+        'J' => 11,
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => panic!("Invalid card"),
+    }
+}
