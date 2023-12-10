3583755670d69649d36b4605f41580e2a4d99887
Alexandr Pleskot <alexandrpleskot@gmail.com>
2023-12-10 16:14:52 +0100

    add rust solution for day 9

---
+++ day_9/first_star/rust/src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    let input = "../../input.txt";
+    let forecat = std::fs::read_to_string(input)
+        .expect("Error reading input file")
+        .lines()
+        .map(|line| {
+            line.split(' ')
+                .map(|f| f.parse::<isize>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let output: isize = forecat.iter().map(|f| get_next(f)).sum();
+    println!("{}", output);
+}
+
+fn get_next(forecast: &[isize]) -> isize {
+    let mut layers: Vec<Vec<_>> = vec![forecast.into()];
+    while layers.last().unwrap().iter().any(|f| *f != 0) {
+        let mut next_layer = Vec::new();
+        for (i, f) in layers.last().unwrap().iter().enumerate() {
+            if i == 0 {
+                continue;
+            }
+            next_layer.push(f - layers.last().unwrap()[i - 1]);
+        }
+        layers.push(next_layer);
+    }
+    let mut next = 0;
+    for (i, f) in layers.iter().rev().enumerate() {
+        if i == 0 {
+            continue;
+        }
+        next += f.last().unwrap();
+    }
+    next
+}
