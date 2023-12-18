07b6227e1b455ec30c0ddf3135f00b6bf531afc0
Alexandr Pleskot <alexandrpleskot@gmail.com>
2023-12-18 10:10:17 +0100

    add rust solution for day 18

--- day_18/second_star/rust/src/main.rs
+++ day_18/second_star/rust/src/main.rs
@@ -1,47 +1,50 @@
 fn main() {
-    let input = "../../input.txt";
+    let input = "../../test.txt";
     let input_file = std::fs::read_to_string(input).expect("Error reading input file");
-    let instructions: Vec<(char, i32)> = input_file
+    let instructions: Vec<(&str, i32)> = input_file
         .lines()
         .map(|line| {
-            let (action, value) = line.split_once(' ').unwrap();
-            (action.chars().next().unwrap(), value.split_once(' ').unwrap().0.parse::<i32>().unwrap())
+            let (_, value) = line.split_once('#').unwrap();
+            let value = value.trim_end_matches(')');
+            let (val, action) = value.split_at(5);
+            let val = i32::from_str_radix(val, 16).unwrap();
+            (action, val)
         })
         .collect();
     let mut holes = vec![(0,0)];
     for instruction in instructions {
         match instruction.0 {
-            'L' => {
+            "2" => {
                 for _ in 0..instruction.1 {
                     holes.push((holes.last().unwrap().0 - 1, holes.last().unwrap().1));
                 }
             },
-            'R' => {
+            "0" => {
                 for _ in 0..instruction.1 {
                     holes.push((holes.last().unwrap().0 + 1, holes.last().unwrap().1));
                 }
             },
-            'U' => {
+            "3" => {
                 for _ in 0..instruction.1 {
                     holes.push((holes.last().unwrap().0, holes.last().unwrap().1 - 1));
                 }
             },
-            'D' => {
+            "1" => {
                 for _ in 0..instruction.1 {
                     holes.push((holes.last().unwrap().0, holes.last().unwrap().1 + 1));
                 }
             },
-            _ => panic!("Unknown instruction")
+            x => panic!("Unknown instruction {}", x)
         }
     }
     holes.pop();
     let min_y = holes.iter().min_by_key(|(_,y)| y).unwrap().1;
     holes.iter_mut().for_each(|(_,y)| *y -= min_y);
-    let count = shoelace(&holes);
+    let count = shoelace_area(&holes);
     println!("{}", count - (holes.len() as i64 / 2) + 1 + holes.len() as i64);
 }
 
-fn shoelace(vertices: &[(i32, i32)]) -> i64 {
+fn shoelace_area(vertices: &[(i32, i32)]) -> i64 {
     let mut sum = 0;
     for i in 0..vertices.len() {
         let (x1, y1) = vertices[i];
